b613b1e60a01cc6f638669faba7d5d235549948e
Lex305 <lex305@web.de>
2022-03-19 21:31:51 +0100

    fix comma separation

--- src/main/java/de/leximon/quartz/commands/ModsCommand.java
+++ src/main/java/de/leximon/quartz/commands/ModsCommand.java
@@ -70,7 +70,7 @@ public class ModsCommand {
             }
             i++;
             if(i != mods.size())
-                b.append(Component.text(metadata.getName(), TextColor.color(0x4296f5)).hoverEvent(HoverEvent.showText(metadataText.build())))
+                b.append(Component.text(metadata.getName(), i % 2 == 0 ? TextColor.color(0x80BEF5) : TextColor.color(0x4296f5)).hoverEvent(HoverEvent.showText(metadataText.build())))
                         .append(Component.text(",", NamedTextColor.WHITE));
         }
         context.getSource().sendFeedback(Quartz.adventure().toNative(b.build()), false);
